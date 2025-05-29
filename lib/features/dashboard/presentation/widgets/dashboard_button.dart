be8b64c176f517c6083402307dcc2925a74c1f25
MaVilla <melissa.villa2019@gmail.com>
2025-05-29 03:59:25 -0500

    agg tittleStyle, mejora userProfile y home, agg Dashboard

---
+++ lib/features/dashboard/presentation/widgets/dashboard_button.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class DashboardButton extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  final VoidCallback onPressed;
+
+  const DashboardButton({
+    Key? key,
+    required this.icon,
+    required this.label,
+    required this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            shape: const CircleBorder(),
+            padding: EdgeInsets.all(16.w),
+            backgroundColor: Colors.green,
+          ),
+          onPressed: onPressed,
+          child: Icon(icon, size: 34.sp, color: Colors.white),
+        ),
+        SizedBox(height: 4.h),
+        Text(
+          label,
+          style: TextStyle(
+            fontSize: 14.sp,
+            fontWeight: FontWeight.w700,
+            color: Colors.black12.withOpacity(0.75),
+          ),
+        ),
+      ],
+    );
+  }
+}
